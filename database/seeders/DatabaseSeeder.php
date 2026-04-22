a2d5c63c746c4ced41460629d465f2e0c1f01ba6
ardut1351 <dagung34@gmail.com>
2026-04-22 18:54:41 +0700

    first commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,8 +4,10 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
+use App\Models\Kategori;
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,24 +16,44 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        
-    User::create([
-    'nama' => 'Administrator',
-    'email' => 'admin@gmail.com',
-    'role' => '1',
-    'status' => 1,
-    'hp' => '0812345678901',
-    'password' => bcrypt('P@55word'),
-    ]);
-    #untuk record berikutnya silahkan, beri nilai berbeda pada nilai: nama, email, hp dengan 
-    User::create([
-    'nama' => 'Sopian Aji',
-    'email' => 'sopian4ji@gmail.com',
-    'role' => '0',
-    'status' => 1,
-    'hp' => '081234567892',
-    'password' => bcrypt('P@55word'),
-    ]);
+        DB::table('user')->insert([
+            [
+                'nama' => 'Administrator',
+                'email' => 'admin@gmail.com',
+                'role' => '1',
+                'status' => 1,
+                'hp' => '0812345678901',
+                'password' => bcrypt('P@55word'),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'nama' => 'Sopian Aji',
+                'email' => 'sopian4ji@gmail.com',
+                'role' => '0',
+                'status' => 1,
+                'hp' => '081234567892',
+                'password' => bcrypt('P@55word'),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        ]);
     
+    #data kategori 
+    Kategori::create([ 
+    'nama_kategori' => 'Brownies', 
+    ]); 
+    Kategori::create([ 
+    'nama_kategori' => 'Combro', 
+    ]); 
+    Kategori::create([ 
+    'nama_kategori' => 'Dawet', 
+    ]); 
+    Kategori::create([ 
+    'nama_kategori' => 'Mochi', 
+    ]); 
+    Kategori::create([ 
+    'nama_kategori' => 'Wingko', 
+    ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use App\Http\Controllers\BerandaController;
+use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\ProdukController;
+use App\Http\Controllers\RajaOngkirController;
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,12 +22,90 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');
+    return redirect()->route('beranda');
 });
 
-Route::get('backend/beranda', [BerandaController::class, 'berandaBackend'])->name('backend.beranda')->middleware('auth');
+Route::get('/beranda', [BerandaController::class, 'index'])->name('beranda');
+
+Route::get('backend/beranda', [BerandaController::class, 'berandaBackend'])
+    ->name('backend.beranda')
+    ->middleware('auth');
 
 Route::get('backend/login', [LoginController::class, 'loginBackend'])->name('backend.login');
 Route::post('backend/login', [LoginController::class, 'authenticateBackend'])->name('backend.login');
 Route::post('backend/logout', [LoginController::class, 'logoutBackend'])->name('backend.logout');
+
+//Route::resource('backend/user', UserController::class)->middleware('auth');
+Route::resource('backend/user', UserController::class, ['as' => 'backend'])->middleware('auth');
+
+Route::resource('backend/kategori', KategoriController::class, ['as' => 'backend'])->middleware('auth');
+
+Route::resource('backend/produk', ProdukController::class, ['as' => 'backend'])->middleware('auth');
+// Route untuk menambahkan foto
+Route::post('foto-produk/store', [ProdukController::class, 'storeFoto'])
+    ->name('backend.foto_produk.store')
+    ->middleware('auth');
+// Route untuk menghapus foto
+Route::delete('foto-produk/{id}', [ProdukController::class, 'destroyFoto'])
+    ->name('backend.foto_produk.destroy')
+    ->middleware('auth');
+
+Route::get('backend/laporan/formuser', [UserController::class, 'formUser'])
+    ->name('backend.laporan.formuser')
+    ->middleware('auth');
+Route::post('backend/laporan/cetakuser', [UserController::class, 'cetakUser'])
+    ->name('backend.laporan.cetakuser')
+    ->middleware('auth');
+
+Route::get('backend/laporan/formproduk', [ProdukController::class, 'formProduk'])
+    ->name('backend.laporan.formproduk')
+    ->middleware('auth');
+Route::post('backend/laporan/cetakproduk', [ProdukController::class, 'cetakProduk'])
+    ->name('backend.laporan.cetakproduk')
+    ->middleware('auth');
+
+Route::get('/produk/detail/{id}', [ProdukController::class, 'detail'])->name('produk.detail');
+
+Route::get('/produk/kategori/{id}', [ProdukController::class, 'produkKategori'])->name('produk.kategori');
+
+Route::get('/produk/all', [ProdukController::class, 'produkAll'])->name('produk.all');
+
+//API Google
+Route::get('/auth/redirect', [CustomerController::class, 'redirect'])->name('auth.redirect');
+Route::get('/auth/google/callback', [CustomerController::class, 'callback'])->name('auth.callback');
+// Logout
+Route::post('/logout', [CustomerController::class, 'logout'])->name('customer.logout');
+
+Route::resource('backend/customer', CustomerController::class, ['as' => 'backend'])->middleware('auth');
+
+// Group route untuk customer
+Route::middleware('is.customer')->group(function () {
+    // Route untuk menampilkan halaman akun customer
+    Route::get('/customer/akun/{id}', [CustomerController::class, 'akun'])->name('customer.akun');
+    // Route untuk mengupdate data akun customer
+    Route::put('/customer/updateakun/{id}', [CustomerController::class, 'updateAkun'])->name('customer.updateakun');
+    // Route untuk menambahkan produk ke keranjang
+    Route::post('add-to-cart/{id}', [OrderController::class, 'addToCart'])->name('order.addToCart');
+    Route::get('cart', [OrderController::class, 'viewCart'])->name('order.cart');
+});
+
+
+Route::get('/list-ongkir', function () {
+    $response = Http::withHeaders([
+        'key' => 'M16Tqpov4298f3546a7fa5b6hZ1ude4c'
+    ])->get('https://rajaongkir.komerce.id/api/v1/destination/province');
+
+  dd($response->json());
+});
+
+Route::get('/cek-ongkir', function () {
+return view('ongkir');
+});
+Route::get('/provinces', [RajaOngkirController::class, 'getProvinces']);
+Route::get('/cities', [RajaOngkirController::class, 'getCities']);
+Route::post('/cost', [RajaOngkirController::class, 'getCost']);
